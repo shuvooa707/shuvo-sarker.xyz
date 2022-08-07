7b20a2fe0a33c26a6e0629659e7a4750e461c88e
shuvo <shuvooa707@gmail.com>
2022-08-07 11:42:32 +0600

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        @font-face {
+            font-family: myFirstFont;
+            src: url( {{ resource_path() . "/fonts/elvencommonspeak.ttf" }});
+        }
+    </style>
 </head>
 <body>
     <h1 style="text-align: center">Life Is Beautiful</h1>    
